7e082c983669ebf43153f62f058a58b6ffd650d7
John Patrick Culi Isidoro <jcisidoro@sscgi.com>
2025-03-04 13:50:56 +0800

    Added footer and updated css

--- index.js
+++ index.js
@@ -6,3 +6,35 @@ document.addEventListener("DOMContentLoaded", function () {
     navLinks.classList.toggle("show-menu");
   });
 });
+
+let lastScrollTop = 0; // Track the last scroll position
+const nav = document.querySelector(".header");
+const navLinks = document.querySelector(".nav-links"); // Get the nav element
+
+window.addEventListener("scroll", function () {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop; // Get the current scroll position
+
+  // If we're scrolling down, hide the navbar
+  if (currentScroll > lastScrollTop) {
+    nav.style.top = "-160px"; // Hide the navbar (adjust 60px based on the navbar height)
+    navLinks.style.left = "-1280px";
+  } else {
+    nav.style.top = "0"; // Show the navbar when scrolling up
+    navLinks.style.left = "0";
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Update the last scroll position
+});
+
+const links = document.querySelectorAll("a");
+const menu = document.querySelector(".nav-links");
+
+// Add event listener for each <a> tag
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    // Check if .show-menu exists and is currently visible
+    if (menu && menu.classList.toggle("show-menu")) {
+      menu.classList.remove("show-menu"); // Remove the class that shows the menu
+    }
+  });
+});
